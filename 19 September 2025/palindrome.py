dc764fd06364590812ab665b311637f3a9b0300b
soorzj <166585708+soorzj@users.noreply.github.com>
2025-09-19 10:38:52 +0530

    Update palindrome.py
    updated variable names

--- 19 September 2025/palindrome.py
+++ 19 September 2025/palindrome.py
@@ -1,17 +1,17 @@
 #wap to check if a number input is palindrome
 a=int(input("Enter a number: "))
 temp=a
-n1=0
+count=0 #digitcount
 while temp>0:
     temp=temp//10
-    n1=n1+1
+    count=count+1
 temp=a
 rev=0
-for i in range (n1):
-    n2=temp%10
-    rev = rev*10+n2
+for i in range (count):
+    rev = rev*10+temp%10
     temp=temp//10
 if(rev==a):
     print(a," is Palindrome")
 else:
+
      print(a," is not Palindrome")
