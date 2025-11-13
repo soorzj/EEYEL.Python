cb55b156c2a33c3cc90afd1dbfdaa260dcb65bff
soorzj <166585708+soorzj@users.noreply.github.com>
2025-11-14 01:19:43 +0530

    Modify dict2.py to accept user input for squares
    Updated the script to take user input for the number of squares to generate and formatted the output.

--- 11 Noveber 2025/dict2.py
+++ 11 Noveber 2025/dict2.py
@@ -1,5 +1,8 @@
 #generate a dictornay of numbers and their squares
-squares={}
-for i in range(1,11):
-    squares[i]=i**2
-print(squares)
+a=int(input("Enter numbers required:"))
+dict={}
+for i in range(1,a+1):
+    dict[i]=i**2
+print("Number\t Square")
+for key in dict:
+    print(key ,"\t", dict[key])
