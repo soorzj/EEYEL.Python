b3e5dbe9938bee834a31606ab2409b41b9ad2e66
soorzj <166585708+soorzj@users.noreply.github.com>
2025-11-14 10:30:10 +0530

    Update palindrome_recursive.py

--- 14 November 2025/palindrome_recursive.py
+++ 14 November 2025/palindrome_recursive.py
@@ -1,5 +1,3 @@
-str=input("Enter the string: ")
-count=len(str)
 def palindrome(str,rev,i):
     if i==len(str):
         if str==rev:
@@ -8,7 +6,11 @@ def palindrome(str,rev,i):
             return False
     else:
         return palindrome(str, rev + str[-(i+1)], i+1)
+
+str=input("Enter the string: ")
+count=len(str)
 if palindrome(str,"",0) == True:
     print("The number is palindrome")
 else:
+
     print("The number is not palindrome")
